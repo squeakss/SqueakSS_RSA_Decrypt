ff8afbb23e07a605e0a78c112653dd3856cd8321
squeakss <86993447+squeakss@users.noreply.github.com>
2024-07-08 22:25:49 -0400

    Decode m

---
+++ Decode m.py
@@ -0,0 +1,7 @@
+# List of ASCII values
+ascii_values = [, , , , , , , , , , , , ]
+
+# Decode ASCII values to characters and join them into a string
+decoded_string = ''.join(chr(value) for value in ascii_values)
+
+print(decoded_string)
